98c1abe50b51c98f663952fc151d502f7ff74f9e
Bloodchai <71493062+Bloodchai@users.noreply.github.com>
2021-04-13 12:24:02 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import BaseView, MarksManagerByDiscipline, MarksManagerByGroup
-
-urlpatterns = [
-    path('', BaseView.as_view(), name='base'),
-    path('<str:slug>_marks/', MarksManagerByDiscipline.as_view(), name='discipline_marks'),
-    path('group_<str:slug>/', MarksManagerByGroup.as_view(), name='group_marks'),
-]
